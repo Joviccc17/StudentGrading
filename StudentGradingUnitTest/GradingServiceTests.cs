1da34689fe6b7f1a62bfbea6b796142681870141
matejkar <69653738+matejkar@users.noreply.github.com>
2026-01-31 00:04:35 +0100

    dodani unit testovi

---
+++ StudentGradingUnitTest/GradingServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentGrading.Services;
+using StudentGrading.Models;
+using System.Collections.Generic;
+
+
+namespace StudentGradingUnitTest
+{
+    [TestClass]
+    public class GradingServiceTests
+    {
+        private GradingService _service = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _service = new GradingService();
+        }
+
+        public void CalculateScore_WhenAnswerIsCorrect_AddsPoints()
+        {
+            var questions = new List<Question>
+            {
+                new Question { Id = 1, CorrectAnswer = "4", Points = 2 }
+            };
+
+            var answers = new Dictionary<int, string>
+            {
+                { 1, "4" }
+            };
+
+            int score = _service.CalculateScore(questions, answers);
+
+            Assert.AreEqual(2, score);
+        }
+
+        public void CalculateScore_WhenAnswerIsWrong_AddsZeroPoints()
+        {
+            var questions = new List<Question>
+            {
+                new Question { Id = 1, CorrectAnswer = "4", Points = 2 }
+            };
+
+            var answers = new Dictionary<int, string>
+            {
+                { 1, "5" }
+            };
+
+            int score = _service.CalculateScore(questions, answers);
+
+            Assert.AreEqual(0, score);
+
+        }
+
+        public void CalculateGrade_WhenPercentageIs90_Returns5()
+        {
+            double percentage = 90;
+
+            int grade = _service.CalculateGrade(percentage);
+
+            Assert.AreEqual(5, grade);
+        }
+
+
+
+
+    }
+
+}
+
+
